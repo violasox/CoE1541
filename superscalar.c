3ce93b81162c577d90fbafbddbfceaaeabd558f0
Kaylene <viola.sox@gmail.com>
2018-10-01 17:51:15 -0400

    Working (?) superscalar

--- superscalar.c
+++ superscalar.c
@@ -11,14 +11,16 @@
 #include <stdlib.h>
 #include <arpa/inet.h>
 #include "CPU.h" 
+#include "superscalar.h"
 
 int main(int argc, char **argv)
 {
-  struct instruction* tr_entry1, tr_entry2;
+  struct instruction* tr_entry1;
+  struct instruction* tr_entry2;
   struct instruction IF1, ID1, EX1, MEM1, WB1;
   struct instruction IF2, ID2, EX2, MEM2, WB2;
   struct instruction packing[2];
-  struct instruction nop {ti_NOP, 0, 0, 0, 0, 0};
+  struct instruction nop = {ti_NOP, 0, 0, 0, 0, 0};
   size_t size;
   char *trace_file_name;
   int trace_view_on = 0;
@@ -61,28 +63,32 @@ int main(int argc, char **argv)
       type1 = 1;
       type2 = 1;
     }
+    // If the second is a branch, it acts like a regular instruction
+    if (type2 == 3) 
+        type2 = 1;
+
     // Can fully pack the superinstruction
     if (type1 != type2) {
       leftover = 0;
       if (type1 == 1) {
-        memcpy(&packing[0], tr_entry1, sizeof(IF));
-        memcpy(&packing[1], tr_entry2, sizeof(IF));
+        memcpy(&packing[0], tr_entry1, sizeof(IF1));
+        memcpy(&packing[1], tr_entry2, sizeof(IF1));
       }
       else {
-        memcpy(&packing[0], tr_entry2, sizeof(IF));
-        memcpy(&packing[1], tr_entry1, sizeof(IF));
+        memcpy(&packing[0], tr_entry2, sizeof(IF1));
+        memcpy(&packing[1], tr_entry1, sizeof(IF1));
       }
     }
     // Superinstruction gets one real instruction and one no-op
     else {
       leftover = 1;
       if (type1 == 1) {
-        memcpy(&packing[0], tr_entry1, sizeof(IF));
-        memcpy(&packing[1], &nop, sizeof(IF));
+        memcpy(&packing[0], tr_entry1, sizeof(IF1));
+        memcpy(&packing[1], &nop, sizeof(IF1));
       }
       else {
-        memcpy(&packing[0], &nop, sizeof(IF));
-        memcpy(&packing[1], tr_entry1, sizeof(IF));
+        memcpy(&packing[0], &nop, sizeof(IF1));
+        memcpy(&packing[1], tr_entry1, sizeof(IF1));
       }
     }
 
@@ -109,8 +115,8 @@ int main(int argc, char **argv)
         flush_counter--;   
       }
       else {
-        memcpy(&IF1, &packing[0], sizeof(IF));
-        memcpy(&IF2, &packing[1], sizeof(IF));
+        memcpy(&IF1, &packing[0], sizeof(IF1));
+        memcpy(&IF2, &packing[1], sizeof(IF2));
       }
 
       //printf("==============================================================================\n");
@@ -118,9 +124,8 @@ int main(int argc, char **argv)
 
 
     if (trace_view_on && cycle_number>=5) {/* print the executed instruction if trace_view_on=1 */
-      printInstruction(WB1, cycle_number);
-      printInstruction(WB2, cycle_number);
-      }
+      printInstruction(WB1, cycle_number, 1);
+      printInstruction(WB2, cycle_number, 2);
     }
   }
 
@@ -129,40 +134,40 @@ int main(int argc, char **argv)
   exit(0);
 }
 
-void printInstruction(struct instruction WB, unsigned int cycle_number) {
+void printInstruction(struct instruction WB, unsigned int cycle_number, int pipeline) {
   switch(WB.type) {
     case ti_NOP:
-      printf("[cycle %d] NOP:\n",cycle_number) ;
+      printf("[cycle %d, pipeline %d] NOP:\n",cycle_number, pipeline) ;
       break;
     case ti_RTYPE: /* registers are translated for printing by subtracting offset  */
-      printf("[cycle %d] RTYPE:",cycle_number) ;
+      printf("[cycle %d, pipeline %d] RTYPE:",cycle_number, pipeline) ;
       printf(" (PC: %d)(sReg_a: %d)(sReg_b: %d)(dReg: %d) \n", WB.PC, WB.sReg_a, WB.sReg_b, WB.dReg);
       break;
     case ti_ITYPE:
-      printf("[cycle %d] ITYPE:",cycle_number) ;
+      printf("[cycle %d, pipeline %d] ITYPE:",cycle_number, pipeline) ;
       printf(" (PC: %d)(sReg_a: %d)(dReg: %d)(addr: %d)\n", WB.PC, WB.sReg_a, WB.dReg, WB.Addr);
       break;
     case ti_LOAD:
-      printf("[cycle %d] LOAD:",cycle_number) ;      
+      printf("[cycle %d, pipeline %d] LOAD:",cycle_number, pipeline) ;      
       printf(" (PC: %d)(sReg_a: %d)(dReg: %d)(addr: %d)\n", WB.PC, WB.sReg_a, WB.dReg, WB.Addr);
       break;
     case ti_STORE:
-      printf("[cycle %d] STORE:",cycle_number) ;      
+      printf("[cycle %d, pipeline %d] STORE:",cycle_number, pipeline) ;      
       printf(" (PC: %d)(sReg_a: %d)(sReg_b: %d)(addr: %d)\n", WB.PC, WB.sReg_a, WB.sReg_b, WB.Addr);
       break;
     case ti_BRANCH:
-      printf("[cycle %d] BRANCH:",cycle_number) ;
+      printf("[cycle %d, pipeline %d] BRANCH:",cycle_number, pipeline) ;
       printf(" (PC: %d)(sReg_a: %d)(sReg_b: %d)(addr: %d)\n", WB.PC, WB.sReg_a, WB.sReg_b, WB.Addr);
       break;
     case ti_JTYPE:
-      printf("[cycle %d] JTYPE:",cycle_number) ;
+      printf("[cycle %d, pipeline %d] JTYPE:",cycle_number, pipeline) ;
       printf(" (PC: %d)(addr: %d)\n", WB.PC,WB.Addr);
       break;
     case ti_SPECIAL:
-      printf("[cycle %d] SPECIAL:\n",cycle_number) ;      	
+      printf("[cycle %d, pipeline %d] SPECIAL:\n",cycle_number, pipeline) ;      	
       break;
     case ti_JRTYPE:
-      printf("[cycle %d] JRTYPE:",cycle_number) ;
+      printf("[cycle %d, pipeline %d] JRTYPE:",cycle_number, pipeline) ;
       printf(" (PC: %d) (sReg_a: %d)(addr: %d)\n", WB.PC, WB.dReg, WB.Addr);
       break;
   }
@@ -175,6 +180,9 @@ int checkInstructionType(enum opcode type) {
       break;
     case ti_STORE:
       return 2;
+      break;
+    case ti_BRANCH:
+      return 3;
       break;
     default:
       return 1; 
